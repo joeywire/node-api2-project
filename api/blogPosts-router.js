119d5ff01a3877666ce23934f570337ae5b0ced0
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-01 15:31:29 -0600

    First enpoint built and functioning properly

--- api/blogPosts-router.js
+++ api/blogPosts-router.js
@@ -4,5 +4,15 @@ const BlogPost = require('../data/db');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    BlogPost.find()
+        .then(bPosts => {
+            res.status(200).json(bPosts); 
+        })
+        .catch(error => {
+            res.status(500).json({ error: "The posts information could not be retrieved."});
+            console.log(error.message);
+        })
+})
 
 module.exports = router; 
