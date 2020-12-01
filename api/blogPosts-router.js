8939432bacf6bb010307bb1dffd77673ef42baca
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-01 16:12:04 -0600

    POST new blogPost added

--- api/blogPosts-router.js
+++ api/blogPosts-router.js
@@ -4,6 +4,23 @@ const BlogPost = require('../data/db');
 
 const router = express.Router();
 
+//POST REQUESTS 
+router.post('/', (req, res) => {
+    //conditional logic to ensure req has necessary info
+    if (!req.body.title || !req.body.contents) { 
+        return res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    BlogPost.insert(req.body)
+        .then(newID => {
+            res.status(201).json(newID);
+        })
+        .catch(err => {
+            res.status(500).json( {errorMessage: err.message});
+        })
+})
+
+
+
 router.get('/', (req, res) => {
     BlogPost.find()
         .then(bPosts => {
