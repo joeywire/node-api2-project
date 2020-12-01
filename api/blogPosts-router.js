753e808bd208d146a660bc4de5ca5a00333d0ad3
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-01 15:16:57 -0600

    Server up and running

---
+++ api/blogPosts-router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+//Import our helper functions to access DB
+const BlogPost = require('../data/db'); 
+
+const router = express.Router();
+
+
+module.exports = router; 
